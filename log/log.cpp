9c1e5e8771b7c89bd2449fc46e7b8b48dac9221c
herehot <870860128@qq.com>
2022-04-11 19:47:56 +0800

    debug in log.cpp

--- log/log.cpp
+++ log/log.cpp
@@ -28,10 +28,10 @@ bool Log::init(const char *file_name, int close_log, int log_buf_size, int split
         m_is_async = true;
         m_log_queue = new block_queue<string>(max_queue_size);
         pthread_t tid;
-        //flush_log_thread为回调函数,这里表示创建线程异步写日志
+        // flush_log_thread为回调函数,这里表示创建线程异步写日志
         pthread_create(&tid, NULL, flush_log_thread, NULL);
     }
-    
+
     m_close_log = close_log;
     m_log_buf_size = log_buf_size;
     m_buf = new char[m_log_buf_size];
@@ -42,7 +42,6 @@ bool Log::init(const char *file_name, int close_log, int log_buf_size, int split
     struct tm *sys_tm = localtime(&t);
     struct tm my_tm = *sys_tm;
 
- 
     const char *p = strrchr(file_name, '/');
     char log_full_name[256] = {0};
 
@@ -58,7 +57,7 @@ bool Log::init(const char *file_name, int close_log, int log_buf_size, int split
     }
 
     m_today = my_tm.tm_mday;
-    
+
     m_fp = fopen(log_full_name, "a");
     if (m_fp == NULL)
     {
@@ -98,16 +97,16 @@ void Log::write_log(int level, const char *format, ...)
     m_mutex.lock();
     m_count++;
 
-    if (m_today != my_tm.tm_mday || m_count % m_split_lines == 0) //everyday log
+    if (m_today != my_tm.tm_mday || m_count % m_split_lines == 0) // everyday log
     {
-        
+
         char new_log[256] = {0};
         fflush(m_fp);
         fclose(m_fp);
         char tail[16] = {0};
-       
+
         snprintf(tail, 16, "%d_%02d_%02d_", my_tm.tm_year + 1900, my_tm.tm_mon + 1, my_tm.tm_mday);
-       
+
         if (m_today != my_tm.tm_mday)
         {
             snprintf(new_log, 255, "%s%s%s", dir_name, tail, log_name);
@@ -120,7 +119,7 @@ void Log::write_log(int level, const char *format, ...)
         }
         m_fp = fopen(new_log, "a");
     }
- 
+
     m_mutex.unlock();
 
     va_list valst;
@@ -133,8 +132,12 @@ void Log::write_log(int level, const char *format, ...)
     int n = snprintf(m_buf, 48, "%d-%02d-%02d %02d:%02d:%02d.%06ld %s ",
                      my_tm.tm_year + 1900, my_tm.tm_mon + 1, my_tm.tm_mday,
                      my_tm.tm_hour, my_tm.tm_min, my_tm.tm_sec, now.tv_usec, s);
-    
-    int m = vsnprintf(m_buf + n, m_log_buf_size - 1, format, valst);
+
+    //缓冲区溢出
+    // debug修改
+    //原行：int m = vsnprintf(m_buf + n, m_log_buf_size - 1, format, valst);
+    int m = vsnprintf(m_buf + n, m_log_buf_size - 1 - n, format, valst);
+    // int m = vsnprintf(m_buf + n, m_log_buf_size - 1, format, valst);
     m_buf[n + m] = '\n';
     m_buf[n + m + 1] = '\0';
     log_str = m_buf;
